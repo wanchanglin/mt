7998fb5bf2af7cf47564069f7b9f7fbf1e3a12d7
harryln <harryln66@hotmail.com>
2021-10-19 09:55:47 +0100

    move mt_deprecated.R, to_do/outl_det.R, mt_util_xlx.r and mt_util_3.R to mt_extra

--- R/mt_util_2.R
+++ R/mt_util_2.R
@@ -69,7 +69,6 @@ if (F) {
 
   tmp.1 <- data.frame(corr, pval, padj)
 }
-
 sym2long <- function(x, tri = c("upper", "lower")) {
   tri <- match.arg(tri)
 
@@ -135,93 +134,6 @@ cor.long <- function(x, use = "pairwise.complete.obs", method = "pearson") {
   return(res)
 }
 
-#' ======================================================================
-#' lwc-08-09-2010: Normalise data set by standardisation
-#' Note: Not documented. The style is consistent with stats.mat.
-preproc.auto <- function(x) {
-  vec.func <- function(x) {
-    res <- (x - mean(x, na.rm = T)) / sd(x, na.rm = T)
-    return(res)
-  }
-
-  x <- as.data.frame(x, stringsAsFactors = F)
-  res <- sapply(x, function(i) vec.func(i))
-  #' res <- colwise(vec.func)(x)
-  res <- as.data.frame(res, stringsAsFactors = FALSE)
-  return(res)
-}
-
-#' ========================================================================
-#' wll-29-07-2015: Transform a data set.
-#' Note: The programming structure is from ststs's 'sd' function. Or S3
-#' programming method is much more prefered.
-#' To-Do: Re-write 'preproc' based on this function
-#' Examples:
-if (F) {
-  data(iris)
-  #' transform an vector
-  vec <- iris[, 1]
-  trans(vec, method = "auto")
-  trans.vec(vec, method = "auto")
-  #' transform a data frame
-  mat <- iris[, 1:4]
-  trans(mat, method = "log")
-  #' transform data frame under condition
-  require(plyr)
-  ddply(iris, ("Species"), function(x, method) {
-    trans(x[, 1:4], method = method)
-  }, method = "range")
-}
-
-trans <- function(x, method = "auto", add = 1) {
-  if (is.matrix(x)) {
-    res <- apply(x, 2, trans.vec, method = method, add = add)
-  } else if (is.vector(x)) {
-    res <- trans.vec(x, method = method, add = add)
-  } else if (is.data.frame(x)) {
-    res <- sapply(x, trans.vec, method = method, add = add)
-  } else {
-    res <- trans.vec(as.vector(x), method = method, add = add)
-  }
-
-  return(res)
-}
-
-#' ========================================================================
-#' wll-29-07-2015: transform an vector
-#' Note: 'add' is only for log transformation
-#' Reference:
-#' Berg, R., Hoefsloot, H., Westerhuis, J., Smilde, A. and Werf, M. (2006),
-#' Centering, scaling, and transformations: improving the biological
-#' information content of metabolomics data, \emph{BMC Genomics}, 7:142
-trans.vec <- function(x, method = "auto", add = 1) {
-  method <- match.arg(method, c(
-    "center", "auto", "range", "pareto", "vast",
-    "level", "log", "log10", "sqrt", "asinh"
-  ))
-
-  #' basic functions
-  me <- function(x) mean(x, na.rm = T)
-  se <- function(x) sd(x, na.rm = T)
-  mx <- function(x) max(x, na.rm = T)
-  mn <- function(x) min(x, na.rm = T)
-  #' sm  <- function(x) sum(x,na.rm=T)
-
-  res <- switch(method,
-    "center"  = (x - me(x)),
-    "auto"    = (x - me(x)) / se(x),
-    "range"   = (x - me(x)) / (mx(x) - mn(x)),
-    "pareto"  = (x - me(x)) / sqrt(se(x)),
-    "vast"    = (x - me(x)) * me(x) / se(x)^2,
-    "level"   = (x - me(x)) / me(x),
-    "log"     = log(x + add),
-    "log10"   = log10(x + add),
-    "sqrt"    = sqrt(x),
-    "asinh"   = asinh(x)
-  )
-  return(res)
-}
-
 #' ========================================================================
 #' lwc-07-07-2011: batch shifting: remove mean withing each batch/block
 #' Arguments:
@@ -256,11 +168,8 @@ rsd <- function(x) {
 #' ========================================================================
 #' lwc-02-06-2011: Relative standard deviation.
 #' lwc-16-06-2011: Re-write according to function sd.
-#' Note: The code style steal from function 'sd'. for details, see:>?sd and
-#' >sd. Comments: This style of recursive is quite cool. May be applied in
-#' the future coding involved in handling with vector, data frame and
-#' matrix. The hint should be writing a vector function and use apply/sapply
-#' for matrix/data frame.
+#' Note: The code style steal from function 'sd', i.e.,  provide a vector 
+#' function and use apply/sapply for matrix/data frame.
 rsd.1 <- function(x, na.rm = TRUE) {
   if (is.matrix(x)) {
     apply(x, 2, rsd.1, na.rm = na.rm)
@@ -960,15 +869,214 @@ incidence.table <- function(id, category, names = NULL, cutoff = 1,
   }
 }
 
+#' ========================================================================
+#' lwc-17-05-2011: Write list of data frame or matrix to Excel's XLS.
+list2xls <- function(x, filename = "tmp.xls", FreezeRow = 1,
+                     row.names = TRUE) {
+  tmp <- names(x)
+  for (i in tmp) assign(i, x[[i]])
+  mt:::WriteXLS(tmp,
+    ExcelFileName = filename, FreezeRow = FreezeRow,
+    row.names = row.names
+  )
+  rm(tmp, list = tmp)
+  #' Remove tmp and variables whose names are included in tmp
+  invisible()
+}
+
+#' ========================================================================
+#' lwc-06-04-2011: Slight modification of WriteXLS.R (2.1.0,2010-09-18) 
+#' Internal function to write data frames to an Excel file using Perl
+WriteXLS <- function(x, ExcelFileName = "R.xls", SheetNames = NULL,
+                     perl = "perl", verbose = FALSE,
+                     Encoding = c("UTF-8", "latin1"), row.names = FALSE,
+                     AdjWidth = FALSE, AutoFilter = FALSE,
+                     BoldHeaderRow = FALSE, FreezeRow = 0, FreezeCol = 0,
+                     envir = parent.frame()) {
+  require("WriteXLS", quietly = TRUE) #' added by LWC
+
+  #' If 'x' is a single name, it is either a single data frame or a list of
+  #' data frames. If 'x' is >1 names in a character vector, it is presumed to
+  #' be a vector of data frame names. If not a list name, create a list of
+  #' data frames from the vector, for consistency in subsequent processing.
+  if (length(x) == 1) {
+    TMP <- get(as.character(x), envir = envir)
+
+    #' is TMP a list and not single data frame
+    if ((is.list(TMP)) & (!is.data.frame(TMP))) {
+      DF.LIST <- TMP
+    } else {
+      DF.LIST <- list(TMP)
+      names(DF.LIST) <- x
+    }
+  } else {
+    DF.LIST <- sapply(as.character(x), function(x) get(x, envir = envir),
+      simplify = FALSE
+    )
+    names(DF.LIST) <- x
+  }
+
+  #' Check to be sure that each element of DF.LIST is a data frame
+
+  #' lwc-06-04-2010: comment the following line for allowing matrix format.
+  #' if (!all(sapply(DF.LIST, is.data.frame))) stop("One or more of the
+  #' objects named in 'x' is not a data frame or does not exist")
+
+  #' Additional checks for Excel 2003 limitations
+  #' 256 columns, including rownames, if included
+  #' 65,536 rows (including header row)
+  if (!all(sapply(DF.LIST, function(x) (nrow(x) <= 65535) & (ncol(x) <= 256)))) {
+    stop("One or more of the data frames named in 'x' exceeds 65535 rows or 256 columns")
+  }
+
+  Encoding <- match.arg(Encoding)
+
+  #' -------------------------------------------------------------------------
+  #'  Check to see if SheetNames is specified and if so: check for
+  #'  duplications they are same length as the number of dataframes CHECK
+  #'  TO see if any SheetNames are >31 chars, which is the Excel Limit
+  #'  check for invalid characters: []:*?/\ ELSE check to see if first 31
+  #'  characters of data frame names are unique
+  if (!is.null(SheetNames)) {
+    if (any(duplicated(SheetNames))) {
+      message("At least one entry in 'SheetNames' is duplicated. Excel worksheets must have unique names.")
+      return(invisible(FALSE))
+    }
+
+    if (length(DF.LIST) != length(SheetNames)) {
+      message("The number of 'SheetNames' specified does not equal the number of data frames in 'x'")
+      return(invisible(FALSE))
+    }
+
+    if (any(nchar(SheetNames) > 31)) {
+      message("At least one of 'SheetNames' is > 31 characters, which is the Excel limit")
+      return(invisible(FALSE))
+    }
+
+    if (any(grep("\\[|\\]|\\*|\\?|:|/|\\\\", SheetNames))) {
+      message("Invalid characters found in at least one entry in 'SheetNames'. Invalid characters are: []:*?/\\")
+      return(invisible(FALSE))
+    }
+    names(DF.LIST) <- SheetNames
+  } else {
+    if (any(duplicated(substr(names(DF.LIST), 1, 31)))) {
+      message("At least one data frame name in 'x' is duplicated up to the first 31 characters. Excel worksheets must have unique names.")
+      return(invisible(FALSE))
+    }
+
+    if (any(grep("\\[|\\]|\\*|\\?|:|/|\\\\", names(DF.LIST)))) {
+      message("Invalid characters found in at least one data frame name in 'x'. Invalid characters are: []:*?/\\")
+      return(invisible(FALSE))
+    }
+  }
+
+  #' Get path to WriteXLS.pl
+  Perl.Path <- file.path(path.package("WriteXLS"), "Perl")
+  Fn.Path <- file.path(Perl.Path, "WriteXLS.pl")
+
+  #' Get path for Tmp.Dir for CSV files
+  Tmp.Dir <- file.path(tempdir(), "WriteXLS")
+
+  #' Remove Tmp.Dir and Files
+  clean.up <- function() {
+    if (verbose) {
+      cat("Cleaning Up Temporary Files and Directory\n\n")
+    }
+    unlink(Tmp.Dir, recursive = TRUE)
+  }
+
+  #' Clean up on function exit
+  on.exit(clean.up())
+
+  #' Cleanup now, in case Tmp.Dir still exists from a prior run
+  if (file.exists(Tmp.Dir)) {
+    if (verbose) {
+      cat("Cleaning Up Temporary Files and Directory From Prior Run\n\n")
+    }
+
+    unlink(Tmp.Dir, recursive = TRUE)
+  }
+
+  #' Create Tmp.Dir for new run
+  if (verbose) {
+    cat("Creating Temporary Directory for CSV Files: ", Tmp.Dir, "\n\n")
+  }
+
+  dir.create(Tmp.Dir, recursive = TRUE)
+
+  #' ---------------------------------------------------------------------
+  #'  Write Comma Delimited CSV files
+  for (i in seq(along = DF.LIST)) {
+    if (verbose) {
+      cat("Creating CSV File: ", i, ".csv", "\n", sep = "")
+    }
+
+    if (row.names) {
+      write.table(DF.LIST[[i]],
+        file = paste(Tmp.Dir, "/", i, ".csv", sep = ""),
+        sep = ",", quote = TRUE, na = "",
+        row.names = TRUE, col.names = NA
+      )
+    } else {
+      write.table(DF.LIST[[i]],
+        file = paste(Tmp.Dir, "/", i, ".csv", sep = ""),
+        sep = ",", quote = TRUE, na = "", row.names = FALSE
+      )
+    }
+  }
+
+  #' Write 'x' (character vector of data frame names) to file
+  #' appending Tmp.Dir and ".csv" to each x
+  x <- paste(Tmp.Dir, "/", seq(length(DF.LIST)), ".csv", sep = "")
+  write(as.matrix(x), file = paste(Tmp.Dir, "/FileNames.txt", sep = ""))
+
+  #' ---------------------------------------------------------------------
+  if (verbose) {
+    cat("Creating SheetNames.txt\n")
+  }
+  write(as.matrix(names(DF.LIST)),
+    file = paste(Tmp.Dir, "/SheetNames.txt", sep = "")
+  )
+
+  if (verbose) {
+    cat("\n")
+  }
+
+  #' Call Perl script
+  cmd <- paste(perl,
+    " -I", shQuote(Perl.Path),
+    " ", shQuote(Fn.Path),
+    " --CSVPath ", shQuote(Tmp.Dir),
+    " --verbose ", verbose,
+    " --AdjWidth ", AdjWidth,
+    " --AutoFilter ", AutoFilter,
+    " --BoldHeaderRow ", BoldHeaderRow,
+    " --FreezeRow ", FreezeRow,
+    " --FreezeCol ", FreezeCol,
+    " --Encoding ", Encoding,
+    " ", shQuote(ExcelFileName),
+    sep = ""
+  )
+
+  #' Call the external Perl script and get the result of the call
+  Result <- system(cmd)
+
+  #' Check to see if Result != 0 in the case of the failure of the Perl
+  #' script This should also raise an error for R CMD check for package
+  #' testing on R-Forge and CRAN
+  if (Result != 0) {
+    message("The Perl script 'WriteXLS.pl' failed to run successfully.")
+    return(invisible(FALSE))
+  } else {
+    return(invisible(TRUE))
+  }
+}
 #' ==================================================
 #' TOC on 25-11-2015
 #' ==================================================
 #' (1). cor.tab
 #' (2). sym2long
 #' (3). cor.long
-#' (4). preproc.auto
-#' (6). trans
-#' (7). trans.vec
 #' (13). batch.shift
 #' (14). rsd
 #' (15). rsd.1
